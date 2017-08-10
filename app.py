7664d22797ef63b051cdd14528648b9692d21681
Paul Davis <pjdavis@gmx.com>
2017-08-10 17:17:14 +0100

    update app

--- app.py
+++ app.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 
 db = MySQLdb.connect("localhost", "root", "yourDbPassWord", "DBname")
 
+def connection():
+    conn = MySQLdb.connect(host="localhost",
+                           user = "root",
+                           passwd = "cookies!",
+                           db = "pythonprogramming")
+    c = conn.cursor()
 
+    return c, conn
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080, passthrough_errors=True)
