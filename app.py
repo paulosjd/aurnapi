18a7cd5f1d33899948bd44565d14a08db6622e88
Paul Davis <pjdavis@gmx.com>
2017-08-10 17:24:36 +0100

    update app

--- app.py
+++ app.py
@@ -14,5 +14,16 @@ def connection():
 
     return c, conn
 
+@app.route('/api/v1.0/items', methods=['GET'])
+def get_items():
+    curs = db.cursor()
+    try:
+        curs.execute("SELECT * FROM items")
+        ...
+
+    except:
+        print "Error: unable to fetch items"
+    return jsonify({"desired: " response})
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080, passthrough_errors=True)
