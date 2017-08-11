1ff06eadc0650da3880baff9de61c66a336c1c3b
Paul Davis <pjdavis@gmx.com>
2017-08-11 12:41:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 from flask import Flask
-import _mysql
+from peewee import *
 
 app = Flask(__name__)
 
-db = MySQLdb.connect("localhost", "root", "yourDbPassWord", "DBname")
-
-def connection():
-    conn = MySQLdb.connect(host="localhost",
-                           user = "root",
-                           passwd = "cookies!",
-                           db = "pythonprogramming")
-    c = conn.cursor()
-
-    return c, conn
-
-@app.route('/api/v1.0/items', methods=['GET'])
-def get_items():
-    curs = db.cursor()
-    try:
-        curs.execute("SELECT * FROM items")
-        ...
-
-    except:
-        print "Error: unable to fetch items"
-    return jsonify({"desired: " response})
+db = SqliteDatabase('aurn-api.db')
 
 
-#db.query("""SELECT spam, eggs, sausage FROM breakfast
- #        WHERE price < 5""")
+class BaseClass(Model):
 
-#r=db.store_result()
+    class Meta:
+        database = db
 
+
+def initialize():
+    """creates table and entry if they don't exist"""
+    db.connect()
+    db.create_tables([Entry], safe=True)
+
+
+class No2Data(BaseClass):
+    site = CharField()
+    no2 = FloatField()
     
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080, passthrough_errors=True)
+    time = CharField()
+
+
+class PmData(BaseClass):
+    site = CharField()
+    pm10 = FloatField()
+    pm25 = FloatField()
+    time = CharField()
+
+
+if __name__ == '__main__':
+    initialize()
