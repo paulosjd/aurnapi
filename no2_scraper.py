c0b91b70882425e2378eba451c31843f607cb256
Paul Davis <pjdavis@gmx.com>
2017-08-13 17:48:13 +0100

    update scraper

--- no2_scraper.py
+++ no2_scraper.py
@@ -11,24 +11,28 @@ ins = {'no2': no2_value}
 no2_value = ''
 data_dict = {site: ins for site in site_list}
 
+ins = {'no2': no2_value}
+no2_value = ''
+data_dict = {site: ins for site in site_list}
+
 site_column[2].text.replace('\xa0', ' ').split(' ')[0]
 
-for site in all_sites:
-    site_link = soup.find_all('a', string=site)[0]
-    site_row = site_link.findParent('td').findParent('tr')
-    site_column = site_row.findAll('td')
-    time_string = site_column[6].text
-    site_name = site_column[0].text
-    o3_value = site_column[1].text.replace('\xa0', ' ').split(' ')[0]
-    no2_value = site_column[2].text.replace('\xa0', ' ').split(' ')[0]
-    so2_value = site_column[3].text.replace('\xa0', ' ').split(' ')[0]
-    pm25_value = site_column[4].text.replace('\xa0', ' ').split(' ')[0]
-    pm10_value = site_column[5].text.replace('\xa0', ' ').split(' ')[0]
-    if no2_value == 'n/a':
-        no2_value = ''
-    ins.update({'no2': no2_value})
+site_link = soup.find_all('a', string=site)[0]
+site_row = site_link.findParent('td').findParent('tr')
+site_column = site_row.findAll('td')
+time_string = site_column[6].text
+site_name = site_column[0].text
+o3_value = site_column[1].text.replace('\xa0', ' ').split(' ')[0]
+no2_value = site_column[2].text.replace('\xa0', ' ').split(' ')[0]
+so2_value = site_column[3].text.replace('\xa0', ' ').split(' ')[0]
+pm25_value = site_column[4].text.replace('\xa0', ' ').split(' ')[0]
+pm10_value = site_column[5].text.replace('\xa0', ' ').split(' ')[0]
+if no2_value == 'n/a':
+    no2_value = ''
+ins.update({'no2': no2_value})
 
 data_dict
+
 """my_dictionary = dict(map(lambda (k,v): (k, f(v)), my_dictionary.iteritems()))"""
 
 }
