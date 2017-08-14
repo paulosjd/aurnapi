8415c1b75cdf04bcffe3c5cfdb6eef996e4f0355
Paul Davis <pjdavis@gmx.com>
2017-08-14 10:21:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,29 +14,36 @@ class Sites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     url = db.Column(db.String(250))
+    lat = Column(String(30))
+    long = Column(String(30))
 
-    def __init__(self, name=None, url=None):
+    def __init__(self, name=None, url=None, lat=None, long=None):
         self.name = name
         self.url = url
+        self.lat = lat
+        self.long = long
+
 
 class Data(db.Model):
     __tablename__ = 'data'
     id = db.Column(db.Integer, primary_key=True)
     site = db.Column(db.String(100), db.ForeignKey('sites.name'))
-    no2 = db.Column(db.Float(10))
-    pm10 = db.Column(db.Float(10))
-    pm25 = db.Column(db.Float(10))
-    time = db.Column(db.String(50))
+    o3 = Column(String(10))
+    no2 = Column(String(10))
+    so2 = Column(String(10))
+    pm10 = Column(String(10))
+    pm25 = Column(String(10))
+    time = Column(String(50))
 
-    def __init__(self, site=None, no2=None, pm10=None, pm25=None, time=None):
+    def __init__(self, site=None, o3=None, no2=None, so2=None, pm10=None, pm25=None, time=None):
         self.site = site
+        self.o3 = o3
         self.no2 = no2
-        self.pm10 = pm10
+        self.so2 = so2
         self.pm25 = pm25
+        self.pm10 = pm10
         self.time = time
-
-db.create_all()
-db.session.add(admin)
+      
 
 """
 db = MySQLdb.connect(host="localhost",
