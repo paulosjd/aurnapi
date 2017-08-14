29fdfec48c55200f8adb865354660c9f53597439
Paul Davis <pjdavis@gmx.com>
2017-08-14 10:22:09 +0100

    Update resources.py

--- resources.py
+++ resources.py
@@ -7,27 +7,32 @@ class Sites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     url = db.Column(db.String(250))
-    lat =
-    long =
+    lat = Column(String(30))
+    long = Column(String(30))
 
-    def __init__(self, name=None, url=None):
+    def __init__(self, name=None, url=None, lat=None, long=None):
         self.name = name
         self.url = url
+        self.lat = lat
+        self.long = long
+
 
 class Data(db.Model):
     __tablename__ = 'data'
     id = db.Column(db.Integer, primary_key=True)
     site = db.Column(db.String(100), db.ForeignKey('sites.name'))
-    o3 = Column(Float(10))
-    no2 = Column(Float(10))
-    so2 = Column(Float(10))
-    pm10 = Column(Float(10))
-    pm25 = Column(Float(10))
+    o3 = Column(String(10))
+    no2 = Column(String(10))
+    so2 = Column(String(10))
+    pm10 = Column(String(10))
+    pm25 = Column(String(10))
     time = Column(String(50))
 
-    def __init__(self, site=None, no2=None, pm10=None, pm25=None, time=None):
+    def __init__(self, site=None, o3=None, no2=None, so2=None, pm10=None, pm25=None, time=None):
         self.site = site
+        self.o3 = o3
         self.no2 = no2
-        self.pm10 = pm10
+        self.so2 = so2
         self.pm25 = pm25
+        self.pm10 = pm10
         self.time = time
