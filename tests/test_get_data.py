4fe18fc4a76c32dfe0c1b71b1b358cd7f4d5a0bb
Paul Davis <pjdavis@gmx.com>
2017-08-21 08:34:16 +0100

    Update test_get_data.py

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -1,69 +1,46 @@
 from get_data import hourly_data, format_data
 from bs4 import BeautifulSoup
+import requests
 import unittest
 from datetime import datetime
-"""
-
->>>print(hourly_data(soup, 'Aberdeen'))
-['46', '1', 'n/m', '3', '6', '20/08/2017 10:00:00']
-
->>>print(format_data(hourly_data(soup, 'Aberdeen')))
-['46', '1', 'n/m', '3', '6', '20/08/2017 10:00:00']
-
->>>foo = hourly_data(soup, 'Aberdeen')[0:5] + ['05/08/2017 14:00:00']
->>>print(foo)
-['46', '1', 'n/m', '3', '6', '05/08/2017 14:00:00']
->>>print(format_data(foo))
-['', '', '', '', '', '20/08/2017 11:00:00']
-"""
 
 
 class TestGetData(unittest.TestCase):
+
     def setUp(self):
-        self.expected_get_data_output = ['39', '18', 'n/m', '6', '12', '19/08/2017 09:00:00']
+        self.page = requests.get('https://uk-air.defra.gov.uk/latest/currentlevels',
+                            headers={'User-Agent': 'Not blank'}).content
+        self.soup = BeautifulSoup(self.page, 'lxml')
 
-    def test_hourly_data(self):
-        self.html_input = BeautifulSoup(data_row, 'lxml')
-        self.assertEqual(hourly_data(self.html_input), self.expected_get_data_output)
+    def test_hourly_data_1(self):
+        ####test that is list containing 6 strings with last item being date time string/match expected format
+        self.output = hourly_data(self.soup, 'Aberdeen')
+        ###self.assert......(hourly_data(self.soup, 'Aberdeen'), )
+        ####self.assertIs([str*6]
 
-    # test that list is returned unaltered assuming that time is up-to-date
+        # test that list item [5] regex date time string matches
+    def test_hourly_data_2(self):
+        self.page = requests.get('https://uk-air.defra.gov.uk/latest/currentlevels',
+                                 headers={'User-Agent': 'Not blank'}).content
+        self.soup = BeautifulSoup(self.page, 'lxml')
+        self.output = hourly_data(self.soup, 'Aberdeen')
+        ####test that is list item [5] regex date time string matches
+        ###self.assert......(hourly_data(self.soup, 'Aberdeen'), )
+        self.assertRegexpMatches
+
+        # test that list is returned unaltered assuming that time is up-to-date
     def test_format_data_1(self):
-        self.assertEqual(format_data(self.expected_get_data_output), self.expected_get_data_output)
-
-    # test that sites with non-up-to-date measurements are handled
-    def test_format_data_2(self):
-        # noinspection PyTypeChecker,PyArgumentList
-        self.expected_output = [''] * 5 + [datetime.strftime((datetime.now().replace(
+        self.get_data_output_1 = ['46', '1', 'n/m', '3', '6'] + [datetime.strftime((datetime.now().replace(
             microsecond=0, second=0, minute=0)), "%d/%m/%Y %H:%M:%S")]
-        self.assertEqual(format_data(self.expected_get_data_output), self.expected_output)
+        self.assertEqual(format_data(self.get_data_output_1), self.get_data_output_1)
+
+        # test that sites with non-up-to-date measurements are handled correctly
+    def test_format_data_2(self):
+        self.get_data_output_1 = ['46', '1', 'n/m', '3', '6', '20/08/2017 10:00:00']
+        self.expected_output_2 = [''] * 5 + [datetime.strftime((datetime.now().replace(
+            microsecond=0, second=0, minute=0)), "%d/%m/%Y %H:%M:%S")]
+        self.assertEqual(format_data(self.get_data_output_1), self.expected_output_2)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-"""
-mock_data =
-[<td><a href="../networks/site-info?site_id=ABD">Aberdeen</a><br/>
- <a class="smalltext" href="https://uk-air.defra.gov.uk/assets/graphs/ABD_weekly_m.png">Timeseries Graph</a></td>,
- <td class="center"><span class="bg_low2 bold">39 (2 Low)</span></td>,
- <td class="center"><span class="bg_low1 bold">18 (1 Low)</span></td>,
- <td class="center"><span title="Not Measured">n/m</span></td>,
- <td class="center"><span class="bg_low1 bold">6 (1 Low)</span></td>,
- <td class="center"><span class="bg_low1 bold">12 (1 Low)
- </span>
- </td>,
- <td>14/08/2017<br/>16:00:00</td>]
- """
-
-"""
-mock_data_wrong_time
-[<td><a href="../networks/site-info?site_id=ABD">Aberdeen</a><br/>
- <a class="smalltext" href="https://uk-air.defra.gov.uk/assets/graphs/ABD_weekly_m.png">Timeseries Graph</a></td>,
- <td class="center"><span class="bg_low2 bold">39 (2 Low)</span></td>,
- <td class="center"><span class="bg_low1 bold">18 (1 Low)</span></td>,
- <td class="center"><span title="Not Measured">n/m</span></td>,
- <td class="center"><span class="bg_low1 bold">6 (1 Low)</span></td>,
- <td class="center"><span class="bg_low1 bold">12 (1 Low)
- </span>
- </td>,
- <td>14/08/2017<br/>12:00:00</td>]
- """
