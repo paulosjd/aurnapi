68c8ac4b6de88438b0cf443d62bc56b21f87e950
Paul Davis <pjdavis@gmx.com>
2017-08-22 13:03:55 +0100

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, Blueprint, jsonify
+from flask import Blueprint, jsonify
 
+#db sessionmaker ??
 
-@get_pm10_blueprint.route('/chart/<site><int:days>')
+@sites_blueprint.route('/chart/<site><int:days>')
 def get_pm10(ab):
     d = make_dict(ab)
     return jsonify(d)
