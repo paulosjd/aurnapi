871bd2807379f9fdaffd449f9d95a12cb4730e09
Paul Davis <pjdavis@gmx.com>
2017-08-23 12:23:49 +0100

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -10,13 +10,13 @@ def hourly_data(soup, site):
     pm25 = row[4].text.replace('\xa0', ' ').split(' ')[0]
     pm10 = row[5].text.replace('\xa0', ' ').split(' ')[0]
     time = row[6].text[:10] + ' ' + row[6].text[10:]
-    return [o3, no2, so2, pm25, pm10, time]
+    return [site, o3, no2, so2, pm25, pm10, time]
 
 
 def format_data(hourly_data_output):
-    if hourly_data_output[5] != datetime.strftime((datetime.now().replace(microsecond=0, second=0, minute=0)),
+    if hourly_data_output[6] != datetime.strftime((datetime.now().replace(microsecond=0, second=0, minute=0)),
                                                   "%d/%m/%Y %H:%M:%S"):
-        return ['n/a'] * 5 + [datetime.strftime((datetime.now().replace(
+        return [hourly_data_output[0]] + ['n/a'] * 5 + [datetime.strftime((datetime.now().replace(
                                                             microsecond=0, second=0, minute=0)), "%d/%m/%Y %H:%M:%S")]
     else:
         return hourly_data_output
