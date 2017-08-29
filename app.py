c59618c5dafbd258b4b6b9dfb980323cb56e6e54
Paul Davis <pjdavis@gmx.com>
2017-08-29 16:15:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from models import db, Sites, Data
+from flask import Flask
+from models import db, Data, Sites
 from bs4 import BeautifulSoup
 from site_metadata import site_list
 from get_data import hourly_data, format_data
@@ -7,6 +7,7 @@ import requests
 
 
 app = Flask(__name__)
+#app.config.from_pyfile('config.py')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
@@ -21,9 +22,11 @@ def populate():
             db.session.add(site_data)
         db.session.commit()
 
-@app.route('/foo')
-def not_found():
-    return jsonify({'foo': 45})
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080, passthrough_errors=True)
+
+"""
+with app.app_context():
+    site_list = Sites.query.all()
+    ab = [a.name for a in site_list]
+    print(ab)
+"""
