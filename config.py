ae2d84278f4c9eca505c21e531231963012f97ae
Paul Davis <pjdavis@gmx.com>
2017-08-29 16:16:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,33 @@
+# config.py
 
 DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
