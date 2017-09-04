6cf26d6c7b0d8c325747b430bd39e774425ba3bf
Paul Davis <pjdavis@gmx.com>
2017-09-04 10:41:22 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,32 +2,5 @@
 
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+#Required for working in PyCharm:
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-
-class Config(object):
-    """
-    Common configurations
-    """
-
-    # Put any configurations here that are common across all environments
-
-class DevelopmentConfig(Config):
-    """
-    Development configurations
-    """
-
-    DEBUG = True
-    SQLALCHEMY_ECHO = True
-
-class ProductionConfig(Config):
-    """
-    Production configurations
-    """
-
-    DEBUG = False
-
-app_config = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig
-}
