a82564b1f2a4bfafc0ef29cbd1d75bd8dd94581a
Paul Davis <pjdavis@gmx.com>
2017-09-04 10:42:08 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class Sites(db.Model):
     __tablename__ = 'sites'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
+    site_code = db.Column(db.String(10))
     region = db.Column(db.String(100))
     environ = db.Column('environment', db.String(100))
     url = db.Column(db.String(250))
@@ -15,8 +16,9 @@ class Sites(db.Model):
     lat = db.Column('latitude', db.String(50))
     long = db.Column('longitude', db.String(50))
 
-    def __init__(self, name, region, environ, url, map_url, lat, long):
+    def __init__(self, name, site_code, region, environ, url, map_url, lat, long):
         self.name = name
+        self.site_code = site_code
         self.region = region
         self.environ = environ
         self.url = url
