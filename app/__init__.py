b619063349dd74fdfe20b0dda95d1ad329e52551
Paul Davis <pjdavis@gmx.com>
2017-09-06 09:23:51 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from .models import db
 
 
-
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    from .views import first_blueprint
-    app.register_blueprint(first_blueprint)
+    from .views.hourly_data import hourly_data
+    from .views.sites_info import sites_info
+    app.register_blueprint(hourly_data)
+    app.register_blueprint(sites_info)
     db.init_app(app)
     with app.app_context():
         return app
