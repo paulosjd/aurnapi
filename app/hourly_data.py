f66860e1df64a6f02a97c474d59c3d7ae9bbba5f
Paul Davis <pjdavis@gmx.com>
2017-09-08 14:27:17 +0100

    Update hourly_data.py

--- app/hourly_data.py
+++ app/hourly_data.py
@@ -1,20 +1,10 @@
 from flask import jsonify, Blueprint
-from app.models import Data, Sites
+from app.models import Data
 import datetime
+
 
 hourly_data = Blueprint('hourly', __name__)
 
-
-
-#@hourly_data.route('/ozone/<site-code>/<timeperiod>')
-#def ozone_data(site-code):
-
-    #    foo = Data.query.filter_by(site=site_code).all()
-    #    return jsonify(foo[0].pm10)
-    #return jsonify(foo[0].pm10)
-
-#    foo = Data.query.filter_by(site=site_code).all()
-#    return jsonify(foo[0].pm10)
 
 @hourly_data.route('/pm10/<name>')
 def all_data(name):
@@ -24,7 +14,6 @@ def all_data(name):
 @hourly_data.route('/pm10/<name>/<int:days>')
 def pm10_data(name, days):
     period = datetime.timedelta(days=days)
-
     foo = Data.query.filter_by(site=name).all()
     return jsonify({a.time: a.pm10 for a in foo})
 
@@ -32,14 +21,3 @@ def pm10_data(name, days):
 def site_pm10(name, pm10value):
     foo = Data.query.filter_by(site=name, pm10=pm10value).all()
     return jsonify({a.time: a.pm10 for a in foo})
-
-
-
-
-
-
-"""@hourly_data.route('/pm10/<site_code>')
-def site_pm10(site_code):
-    foo = Data.query.filter_by(site_code=site_code).all()
-    return jsonify(foo[0].pm10)"""
-#once done all make way to do '/<pollutant>/.... instead repeating routes for each pollutant
