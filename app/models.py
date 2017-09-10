1c3c7330ad9bd19f3b96d6f954f75946fbe2488f
Paul Davis <pjdavis@gmx.com>
2017-09-10 15:19:00 +0100

    update models

--- app/models.py
+++ app/models.py
@@ -30,8 +30,7 @@ class Sites(db.Model):
 class Data(db.Model):
     __tablename__ = 'data'
     id = db.Column(db.Integer, primary_key=True)
-    site = db.Column(db.String(100))
-    #site_code = db.Column(db.String(100))
+    site_code = db.Column(db.String(100))
     ozone = db.Column(db.String(10))
     no2 = db.Column(db.String(10))
     so2 = db.Column(db.String(10))
