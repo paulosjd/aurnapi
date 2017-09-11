2aaa989a8fad2d21946f6b7f17ea439ae244959e
Paul Davis <pjdavis@gmx.com>
2017-09-11 10:40:52 +0100

    Update hourly_data.py

--- app/views/hourly_data.py
+++ app/views/hourly_data.py
@@ -5,34 +5,26 @@ from app.models import Data
 hourly_data = Blueprint('hourly', __name__)
 
 
-@hourly_data.route('/pm10/<name>/')
-def all_data1(name):
-    foo = Data.query.filter_by(site=name)
-    return jsonify({a.time: a.pm10 for a in foo})
+@hourly_data.route('/<pollutant>/<name>/')
+def hourly_data_1(pollutant, name):
+    queryset = Data.query.filter_by(site_code=name)
+    return jsonify({a.time: getattr(a, pollutant) for a in queryset})
 
-@hourly_data.route('/pm10/<name>/<start>/')
-def all_data3(name, start):
+@hourly_data.route('/<pollutant>/<name>/<start>/')
+def hourly_data_2(pollutant, name, start):
     date = reversed(start.split('-'))
     start_time = '{}/{}/{} 00:00:00'.format(*date)
-    foo = Data.query.filter(Data.site == name, Data.time >= start_time)
-    return jsonify({a.time: a.pm10 for a in foo})
+    queryset = Data.query.filter(Data.site_code == name, Data.time >= start_time)
+    return jsonify({a.time: getattr(a, pollutant) for a in queryset})
 
-@hourly_data.route('/pm10/<name>/<start>/<end>')
-def all_data2(name, start, end):
-    #e.g. start = 07-09-2017
+@hourly_data.route('/<pollutant>/<name>/<start>/<end>')
+def hourly_data_3(pollutant, name, start, end):
     start_date = reversed(start.split('-'))
     end_date = reversed(end.split('-'))
     start_time = '{}/{}/{} 00:00:00'.format(*start_date)
     end_time = '{}/{}/{} 00:00:00'.format(*end_date)
-    foo = Data.query.filter(Data.site == name, Data.time >= start_time, Data.time <= end_time)
-    return jsonify({a.time: a.pm10 for a in foo})
-
-
-@hourly_data.route('/<pollutant>/<name>/')
-def all_data4(pollutant, name):
-    foo = Data.query.filter_by(site=name)
-    return jsonify({a.time: getattr(a, pollutant) for a in foo})
-
+    queryset = Data.query.filter(Data.site_code == name, Data.time >= start_time, Data.time <= end_time)
+    return jsonify({a.time: getattr(a, pollutant) for a in queryset})
 
 
 
