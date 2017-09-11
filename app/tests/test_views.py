0e2e3c1e9afbb1fcfa480a44a31fd0ba1bc11414
Paul Davis <pjdavis@gmx.com>
2017-09-11 10:41:38 +0100

    Update test_views.py

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,13 +1,29 @@
+import unittest
+
+from app import create_app
+from app.models import db, Data
+
+app = create_app()
+
+d = Data(site_code='ABD', ozone='10', no2='20', so2='30', pm25='40', pm10='50', time='10/09/2017 15:00:00')
+
+def _init_database():
+    db.session.add(d)
+    db.session.commit(d)
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        db.create_all()
+        _init_database()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 
-
-
-"""from flask_testing import TestCase
-from app import create_app, db
-
-class MyTest(TestCase):
-
-    SQLALCHEMY_DATABASE_URI = 'sqlite://'
-    TESTING = True
-
-    def create_app(self):"""
+if __name__ == '__main__':
+    unittest.main()
