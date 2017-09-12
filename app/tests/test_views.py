eab6b5955a1edc8b01c1b136b61ffc3e5d9ee316
Paul Davis <pjdavis@gmx.com>
2017-09-12 09:42:31 +0100

    Update test_views.py

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,28 +1,28 @@
 import unittest
-
-from app import create_app
+from app import create_test_app
 from app.models import db, Data
 
-app = create_app()
-
-d = Data(site_code='ABD', ozone='10', no2='20', so2='30', pm25='40', pm10='50', time='10/09/2017 15:00:00')
-
-def _init_database():
-    db.session.add(d)
-    db.session.commit(d)
 
 class TestCase(unittest.TestCase):
 
     def setUp(self):
-        app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-        self.app = app.test_client()
+        self.app = create_test_app()
+        self.client = self.app.test_client()
+        db.init_app(self.app)
         db.create_all()
-        _init_database()
+
+
+    def test_db_entry(self):
+        d = Data(site_code='ABD', ozone='10', no2='20', so2='30', pm25='40', pm10='50', time='10/09/2017 15:00:00')
+        db.session.add(d)
+        db.session.commit()
+        self.assertGreater(d.id, 0)
+
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
 
 
 if __name__ == '__main__':
