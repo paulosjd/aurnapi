a1e45b632ac18fbd4a3c42e40f79182142481a58
Paul Davis <pjdavis@gmx.com>
2017-09-15 11:48:06 +0100

    Update hourly_data.py

--- app/views/hourly_data.py
+++ app/views/hourly_data.py
@@ -1,5 +1,5 @@
 from flask import jsonify, Blueprint
-from app.models import Data
+from app.models import Data, Site
 
 
 hourly_data = Blueprint('hourly', __name__)
@@ -7,14 +7,14 @@ hourly_data = Blueprint('hourly', __name__)
 
 @hourly_data.route('/<pollutant>/<name>/')
 def hourly_data_1(pollutant, name):
-    queryset = Data.query.filter_by(site_code=name)
+    queryset = Data.query.join(Site).filter(Site.site_code == name)
     return jsonify({a.time: getattr(a, pollutant) for a in queryset})
 
 @hourly_data.route('/<pollutant>/<name>/<start>/')
 def hourly_data_2(pollutant, name, start):
     date = reversed(start.split('-'))
     start_time = '{}/{}/{} 00:00:00'.format(*date)
-    queryset = Data.query.filter(Data.site_code == name, Data.time >= start_time)
+    queryset = Data.query.join(Site).filter(Site.site_code == name, Data.time >= start_time)
     return jsonify({a.time: getattr(a, pollutant) for a in queryset})
 
 @hourly_data.route('/<pollutant>/<name>/<start>/<end>')
@@ -23,5 +23,5 @@ def hourly_data_3(pollutant, name, start, end):
     end_date = reversed(end.split('-'))
     start_time = '{}/{}/{} 00:00:00'.format(*start_date)
     end_time = '{}/{}/{} 00:00:00'.format(*end_date)
-    queryset = Data.query.filter(Data.site_code == name, Data.time >= start_time, Data.time <= end_time)
+    queryset = Data.query.join(Site).filter(Site.site_code == name, Data.time >= start_time, Data.time <= end_time)
     return jsonify({a.time: getattr(a, pollutant) for a in queryset})
