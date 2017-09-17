0f1e8816b79ba3815aa8bfada5c04d5352927d91
Paul Davis <pjdavis@gmx.com>
2017-09-17 10:11:02 +0100

    update tests

--- app/tests/test_get_data.py
+++ app/tests/test_get_data.py
@@ -1,5 +1,4 @@
 from app.data.hourly import hourly_data, validate_data
-from app.data import hourly
 from bs4 import BeautifulSoup
 import requests
 import unittest
@@ -17,18 +16,18 @@ class TestGetData(unittest.TestCase):
     def test_hourly_data_1(self):
         self.assertEquals([type(a) for a in self.hourly_data_output], [str] * 6)
 
-        # test that last list item [5] is a datetime string with the correct format
+        # test that last list item is a datetime string with the correct format
     def test_hourly_data_2(self):
         self.to_datetime = datetime.strptime(self.hourly_data_output[5], '%d/%m/%Y %H:%M:%S')
         self.assertIsInstance(self.to_datetime, datetime)
 
-        # test that list is returned unaltered if time is up-to-date
+        # test that the list of values is returned unaltered if the site time is up-to-date
     def test_validate_data_1(self):
         self.get_data_output_1 = ['46', '1', 'n/m', '3', '6'] + [datetime.strftime((datetime.now().replace(
             microsecond=0, second=0, minute=0)), "%d/%m/%Y %H:%M:%S")]
         self.assertEqual(validate_data(self.get_data_output_1), self.get_data_output_1)
 
-        # test that sites with non-up-to-date measurements are handled correctly
+        # test that sites with non up-to-date measurements (stale data) are handled correctly
     def test_validate_data_2(self):
         self.get_data_output_2 = ['46', '1', 'n/m', '3', '6', '20/08/2017 10:00:00']
         self.expected_output_2 = ['n/a'] * 5 + [datetime.strftime((datetime.now().replace(
