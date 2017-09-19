eda1e2da821c4522c49b4c17c59af22c25e383fb
Paul Davis <pjdavis@gmx.com>
2017-09-19 08:19:25 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,3 +2,9 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
 # Required for working in PyCharm:
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+#Production settings
+"""
+DEBUG = False
+SQLALCHEMY_DATABASE_URI = 'dialect+driver://aurn_data.db'
+"""
