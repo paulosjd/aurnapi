37c58b1a53ae4317ed1a0067babb02d0e7559fa9
Paul Davis <pjdavis@gmx.com>
2017-09-22 16:22:34 +0100

    Update hourly_data.py

--- app/views/hourly_data.py
+++ app/views/hourly_data.py
@@ -4,13 +4,13 @@ from app.models import Data, Site
 hourly_data = Blueprint('hourly', __name__)
 
 
-@hourly_data.route('/<pollutant>/<name>/')
+@hourly_data.route('/data/<pollutant>/<name>/')
 def hourly_data_1(pollutant, name):
     queryset = Data.query.join(Site).filter(Site.site_code == name)
     return jsonify({a.time: getattr(a, pollutant) for a in queryset})
 
 
-@hourly_data.route('/<pollutant>/<name>/<start>/')
+@hourly_data.route('/data/<pollutant>/<name>/<start>/')
 def hourly_data_2(pollutant, name, start):
     date = reversed(start.split('-'))
     start_time = '{}/{}/{} 00:00:00'.format(*date)
@@ -18,7 +18,7 @@ def hourly_data_2(pollutant, name, start):
     return jsonify({a.time: getattr(a, pollutant) for a in queryset})
 
 
-@hourly_data.route('/<pollutant>/<name>/<start>/<end>')
+@hourly_data.route('/data/<pollutant>/<name>/<start>/<end>')
 def hourly_data_3(pollutant, name, start, end):
     start_date = reversed(start.split('-'))
     end_date = reversed(end.split('-'))
