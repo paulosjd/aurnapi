fc9547d5562cd3b25be3148fad91271faea4081d
Paul Davis <pjdavis@gmx.com>
2017-09-22 10:31:24 +0100

    Update sites_info.py

--- app/views/sites_info.py
+++ app/views/sites_info.py
@@ -4,7 +4,14 @@ from app.models import Site, Data
 sites_info = Blueprint('Site', __name__)
 
 
-@sites_info.route('/site-list')
+# site-list/urban-traffic RETURNS ALL 123 SITES
+# BUT site-list/urban-traffic/<REGION> DOES NOT
+
+# concisify the following using getattr()??  --for a.environ and a.region
+# remove the 2 lists??
+#   - instead of e.g. if region in regions:, just have if hasattr(): ?
+
+@sites_info.route('/site-list/')
 @sites_info.route('/site-list/<environ>')
 @sites_info.route('/site-list/<region>')
 def site_list(environ=None, region=None):
@@ -15,14 +22,36 @@ def site_list(environ=None, region=None):
                'highlands', 'scottish-borders', 'central-scotland']
     if environ in site_environs:
         return jsonify({a.name: a.site_code for a in Site.query.filter_by(environ=environ).all()})
-    if environ == 'site-environments':
-        return jsonify({a.name: a.environ for a in Site.query.all()})
     if region in regions:
         return jsonify({a.name: a.site_code for a in Site.query.filter_by(region=region).all()})
-    if region == 'site-regions':
-        return jsonify({a.name: a.region for a in Site.query.all()})
     else:
         return jsonify({a.name: a.site_code for a in Site.query.all()})
+
+
+@sites_info.route('/<site_code>')
+def site_row(site_code):
+    site = Site.query.filter_by(site_code=site_code).first()
+    site_dict = {'site name': site.name, 'region': site.region, 'site environment': site.environ,
+                 'site url': site.url,
+                 'map url': site.map_url, 'latitude': site.lat, 'longitude': site.long}
+    return jsonify(site_dict)
+
+
+# getattr(a, pollutant) is equivalent to a.pollutant (where 'a' has no attribute pollutant)
+@sites_info.route('/available-data/<pollutant>/<site_code>')
+def available_data(pollutant, site_code):
+    qs = Site.query.join(Data).filter(Site.site_code == site_code).first()
+    times = []
+    for a in qs.data:
+        try:
+            if int(getattr(a, pollutant)):
+                times.append(a.time)
+        except ValueError:
+            continue
+    try:
+        return jsonify([str(len(times)), times[0], times[-1]])
+    except IndexError:
+        return jsonify('no data')
 
 
 @sites_info.route('/site-geo')
@@ -40,27 +69,3 @@ def site_info():
 def site_maps():
     map_urls = Site.query.all()
     return jsonify({a.name: a.map_url for a in map_urls})
-
-
-@sites_info.route('/<site_code>')
-def site_row(site_code):
-    site = Site.query.filter_by(site_code=site_code).first()
-    site_dict = {'site name': site.name, 'region': site.region, 'site environment': site.environ, 'site url': site.url,
-                 'map url': site.map_url, 'latitude': site.lat, 'longitude': site.long}
-    return jsonify(site_dict)
-
-#getattr(a, pollutant) == a.pollutant
-@sites_info.route('/available_data/<pollutant>/<site_code>')
-def available_data(pollutant, site_code):
-    qs = Site.query.join(Data).filter(Site.site_code == site_code).first()
-    timepoints = []
-    for a in qs.data:
-        try:
-            if int(getattr(a, pollutant)):
-                timepoints.append(a.time)
-        except ValueError:
-            continue
-    try:
-        return jsonify([str(len(timepoints)), timepoints[0], timepoints[-1]])
-    except IndexError:
-        return jsonify('no data')
