d984c77735c15a6af1481f7d419f8a6c23ad5e2c
Paul Davis <pjdavis@gmx.com>
2017-09-22 16:23:29 +0100

    Update sites_info.py

--- app/views/sites_info.py
+++ app/views/sites_info.py
@@ -9,61 +9,7 @@ sites_info = Blueprint('Site', __name__)
 def site_list():
     return jsonify({a.name: a.site_code for a in Site.query.all()})
 
-@sites_info.route('/site-regions')
-def site_regions():
-    return jsonify(list(set([a.region for a in Site.query.all()])))
 
-@sites_info.route('/site-environments')
-def site_environs():
-    return jsonify(list(set([a.environ for a in Site.query.all()])))
-
-@sites_info.route('/site-environments')
-@sites_info.route('/site-list/<region>')
-def site_list(environ=None, region=None):
-
-site_environs = ['urban-traffic', 'urban-industrial', 'urban-background', 'suburban-background',
-                 'suburban-industrial', 'rural-background']
-regions = ['greater-london', 'south-east', 'east-midlands', 'eastern', 'yorkshire', 'north-east', 'north-west',
-           'west-midlands', 'south-west', 'south-wales', 'northern-ireland', 'north-east-scotland', 'north-wales',
-           'highlands', 'scottish-borders', 'central-scotland']
-try:
-    filt = getattr()
-
-if environ in site_environs:
-    return jsonify({a.name: a.site_code for a in Site.query.filter_by(environ=environ).all()})
-if region in regions:
-    return jsonify({a.name: a.site_code for a in Site.query.filter_by(region=region).all()})
-
-
-
-"""
-@sites_info.route('/site-list/')
-@sites_info.route('/site-list/<environ>')
-@sites_info.route('/site-list/<region>')
-def site_list(environ=None, region=None):
-    site_environs = ['urban-traffic', 'urban-industrial', 'urban-background', 'suburban-background',
-                     'suburban-industrial', 'rural-background']
-    regions = ['greater-london', 'south-east', 'east-midlands', 'eastern', 'yorkshire', 'north-east', 'north-west',
-               'west-midlands', 'south-west', 'south-wales', 'northern-ireland', 'north-east-scotland', 'north-wales',
-               'highlands', 'scottish-borders', 'central-scotland']
-    if environ in site_environs:
-        return jsonify({a.name: a.site_code for a in Site.query.filter_by(environ=environ).all()})
-    if region in regions:
-        return jsonify({a.name: a.site_code for a in Site.query.filter_by(region=region).all()})
-    else:
-        return jsonify({a.name: a.site_code for a in Site.query.all()})
-        """
-
-@sites_info.route('/<site_code>')
-def site_row(site_code):
-    site = Site.query.filter_by(site_code=site_code).first()
-    site_dict = {'site name': site.name, 'region': site.region, 'site environment': site.environ,
-                 'site url': site.url,
-                 'map url': site.map_url, 'latitude': site.lat, 'longitude': site.long}
-    return jsonify(site_dict)
-
-
-# getattr(a, pollutant) is equivalent to a.pollutant (where 'a' has no attribute pollutant)
 @sites_info.route('/available-data/<pollutant>/<site_code>')
 def available_data(pollutant, site_code):
     qs = Site.query.join(Data).filter(Site.site_code == site_code).first()
@@ -80,13 +26,40 @@ def available_data(pollutant, site_code):
         return jsonify('no data')
 
 
+@sites_info.route('/site-regions')
+@sites_info.route('/site-regions/<region>')
+def site_regions(region=None):
+    if region:
+        return jsonify({a.name: a.site_code for a in Site.query.filter_by(region=region).all()})
+    else:
+        return jsonify(list(set([a.region for a in Site.query.all()])))
+
+
+@sites_info.route('/site-environments')
+@sites_info.route('/site-environments/<environ>')
+def site_environs(environ=None):
+    if environ:
+        return jsonify({a.name: a.site_code for a in Site.query.filter_by(environ=environ).all()})
+    else:
+        return jsonify(list(set([a.environ for a in Site.query.all()])))
+
+
+@sites_info.route('/site-info/<site_code>')
+def site_row(site_code):
+    site = Site.query.filter_by(site_code=site_code).first()
+    site_dict = {'site name': site.name, 'region': site.region, 'site environment': site.environ,
+                 'site url': site.url,
+                 'map url': site.map_url, 'latitude': site.lat, 'longitude': site.long}
+    return jsonify(site_dict)
+
+
 @sites_info.route('/site-geo')
 def site_geo():
     return jsonify({a.name: a.lat + ", " + a.long for a in Site.query.all()})
 
 
-@sites_info.route('/site-info')
-def site_info():
+@sites_info.route('/site-url')
+def site_url():
     site_urls = Site.query.all()
     return jsonify({a.name: a.url for a in site_urls})
 
