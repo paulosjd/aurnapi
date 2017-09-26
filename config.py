ca177bfc77d6e0e9dcc2546426271fd73c4f5b53
Paul Davis <pjdavis@gmx.com>
2017-09-26 09:04:47 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 
 class DevelopmentConfig:
     DEBUG = True
-    TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     # For working in PyCharm:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class TestingConfig:
-    DEBUG = False
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
