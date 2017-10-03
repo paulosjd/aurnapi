b68e05cc77196576b4fd9154009157184bb85fd5
Paul Davis <pjdavis@gmx.com>
2017-10-03 10:28:04 +0100

    Update hourly_data.py

--- app/views/hourly_data.py
+++ app/views/hourly_data.py
@@ -1,18 +1,18 @@
 from flask import jsonify, Blueprint
 from app.models import Data, Site
 
-
 hourly_data = Blueprint('hourly', __name__)
 
 parameters = {'ozone': 'ozone, µg/m3', 'NO2': 'nitrogen dioxide, µg/m3', 'SO2': 'sulfur dioxide, µg/m3',
-'PM25': 'PM2.5 particles, µg/m3', 'PM10': 'PM10 particles, µg/m3'}
+              'PM25': 'PM2.5 particles, µg/m3', 'PM10': 'PM10 particles, µg/m3'}
 
 
 @hourly_data.route('/data/<pollutant>/<name>/')
 def hourly_data_1(pollutant, name):
     queryset = Data.query.join(Site).filter(Site.site_code == name.upper())
-    return jsonify({'data': [{'time': a.time, 'value': getattr(a, pollutant.lower(), a.pm10)} for a in queryset]})
-                                    
+    return jsonify({'data': [{'time': a.time, 'parameter': parameters.get(getattr(a, pollutant.lower())),
+                              'value': getattr(a, pollutant.lower(), a.pm10)} for a in queryset]})
+
 
 @hourly_data.route('/data/<pollutant>/<name>/<start>/')
 def hourly_data_2(pollutant, name, start):
@@ -22,7 +22,8 @@ def hourly_data_2(pollutant, name, start):
     except IndexError:
         return jsonify({'data': None})
     queryset = Data.query.join(Site).filter(Site.site_code == name.upper(), Data.time >= start)
-    return jsonify({'data': [{'time': a.time, 'parameter':   'value': getattr(a, pollutant.lower(), a.pm10)} for a in queryset]})
+    return jsonify({'data': [{'time': a.time, 'parameter': parameters.get(getattr(a, pollutant.lower())),
+                              'value': getattr(a, pollutant.lower(), a.pm10)} for a in queryset]})
 
 
 @hourly_data.route('/data/<pollutant>/<name>/<start>/<end>')
@@ -35,7 +36,8 @@ def hourly_data_3(pollutant, name, start, end):
     except IndexError:
         return jsonify({'data': None})
     queryset = Data.query.join(Site).filter(Site.site_code == name.upper(), Data.time >= start, Data.time <= end)
-    return jsonify({'data': [{'time': a.time, 'value': getattr(a, pollutant.lower(), a.pm10)} for a in queryset]})
+    return jsonify({'data': [{'time': a.time, 'parameter': parameters.get(getattr(a, pollutant.lower())),
+                              'value': getattr(a, pollutant.lower(), a.pm10)} for a in queryset]})
 
 
 @hourly_data.route('/data/pollutants')
