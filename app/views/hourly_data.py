bdcb32e31980a879fe82854637052a666aafab93
Paul Davis <pjdavis@gmx.com>
2017-10-03 08:44:03 +0100

    Update hourly_data.py

--- app/views/hourly_data.py
+++ app/views/hourly_data.py
@@ -3,40 +3,38 @@ from app.models import Data, Site
 
 hourly_data = Blueprint('hourly', __name__)
 
-parameters = {'ozone': 'ozone, µg/m3', 'NO2': 'nitrogen dioxide, µg/m3', 'SO2': 'sulfur dioxide, µg/m3',
-              'PM25': 'PM2.5 particles, µg/m3', 'PM10': 'PM10 particles, µg/m3'}
-
 
 @hourly_data.route('/data/<pollutant>/<name>/')
 def hourly_data_1(pollutant, name):
     queryset = Data.query.join(Site).filter(Site.site_code == name.upper())
-    return jsonify({'data': [{'time': a.time, 'value': getattr(a, pollutant.upper(), a.PM10)} for a in queryset]})
-
+    return jsonify({'data': [{'time': a.time, 'value': getattr(a, pollutant.lower(), a.pm10)} for a in queryset]})
+                                    
 
 @hourly_data.route('/data/<pollutant>/<name>/<start>/')
 def hourly_data_2(pollutant, name, start):
     date = reversed(start.split('-'))
     try:
-        start_time = '{}/{}/{} 00:00:00'.format(*date)
+        start = '{}/{}/{} 00:00:00'.format(*date)
     except IndexError:
         return jsonify({'data': None})
-    queryset = Data.query.join(Site).filter(Site.site_code == name.upper(), Data.time >= start_time)
-    return jsonify({'data': [{'time': a.time, 'value': getattr(a, pollutant.upper(), a.PM10)} for a in queryset]})
-
+    queryset = Data.query.join(Site).filter(Site.site_code == name.upper(), Data.time >= start)
+    return jsonify({'data': [{'time': a.time, 'value': getattr(a, pollutant.lower(), a.pm10)} for a in queryset]})
+                        #  INCLUDE KEY/VALUE  FOR  PARAMETER
 
 @hourly_data.route('/data/<pollutant>/<name>/<start>/<end>')
 def hourly_data_3(pollutant, name, start, end):
-    start_date = reversed(start.split('-'))
-    end_date = reversed(end.split('-'))
+    start = reversed(start.split('-'))
+    end = reversed(end.split('-'))
     try:
-        start_time = '{}/{}/{} 00:00:00'.format(*start_date)
-        end_time = '{}/{}/{} 00:00:00'.format(*end_date)
+        start = '{}/{}/{} 00:00:00'.format(*start)
+        end = '{}/{}/{} 00:00:00'.format(*end)
     except IndexError:
         return jsonify({'data': None})
-    queryset = Data.query.join(Site).filter(Site.site_code == name.upper(), Data.time >= start_time, Data.time <= end_time)
-    return jsonify({'data': [{'time': a.time, 'value': getattr(a, pollutant.upper(), a.PM10)} for a in queryset]})
+    queryset = Data.query.join(Site).filter(Site.site_code == name.upper(), Data.time >= start, Data.time <= end)
+    return jsonify({'data': [{'time': a.time, 'value': getattr(a, pollutant.lower(), a.pm10)} for a in queryset]})
 
-  
+
 @hourly_data.route('/data/pollutants')
 def pollutants():
-    return jsonify({a: b for b, a in parameters.items()})
+    return jsonify({'ozone, µg/m3': 'ozone', 'nitrogen dioxide, µg/m3': 'no2', 'sulfur dioxide, µg/m3': 'so2',
+                    'PM2.5 particles, µg/m3': 'pm25', 'PM10 particles, µg/m3': 'pm10'})
