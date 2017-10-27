8edbe71d98cf7fe4b46328790599af8a3538188f
Maltronic <Maltronic@users.noreply.github.com>
2017-10-27 16:58:04 +0100

    Handle case when site cannot be found

--- app/data/hourly.py
+++ app/data/hourly.py
@@ -6,20 +6,21 @@ import requests
 
 
 def hourly_data(soup, site):
-    site_link = soup.find_all('a', string=site)[0]
-    row = site_link.findParent('td').findParent('tr').findAll('td')
-    o3 = row[1].text.replace('\xa0', ' ').split(' ')[0]
-    no2 = row[2].text.replace('\xa0', ' ').split(' ')[0]
-    so2 = row[3].text.replace('\xa0', ' ').split(' ')[0]
-    pm25 = row[4].text.replace('\xa0', ' ').split(' ')[0]
-    pm10 = row[5].text.replace('\xa0', ' ').split(' ')[0]
-    time = row[6].text[:10] + ' ' + row[6].text[10:]
-    return {'o3': o3, 'no2': no2, 'so2': so2, 'pm25': pm25, 'pm10': pm10, 'time': time}
+    if soup.find_all('a', string=site):
+        site_link = soup.find_all('a', string=site)[0]
+        row = site_link.findParent('td').findParent('tr').findAll('td')
+        o3 = row[1].text.replace('\xa0', ' ').split(' ')[0]
+        no2 = row[2].text.replace('\xa0', ' ').split(' ')[0]
+        so2 = row[3].text.replace('\xa0', ' ').split(' ')[0]
+        pm25 = row[4].text.replace('\xa0', ' ').split(' ')[0]
+        pm10 = row[5].text.replace('\xa0', ' ').split(' ')[0]
+        time = row[6].text[:10] + ' ' + row[6].text[10:]
+        return {'o3': o3, 'no2': no2, 'so2': so2, 'pm25': pm25, 'pm10': pm10, 'time': time}
 
 
 def validate_data(data_dict):
     hourly_dt = datetime.strftime(datetime.now().replace(microsecond=0, second=0, minute=0), "%d/%m/%Y %H:%M:%S")
-    if data_dict['time'] != hourly_dt:
+    if data_dict and data_dict['time'] != hourly_dt:
         na_values = ['n/a'] * 5 + [hourly_dt]
         return dict(zip(['o3', 'no2', 'so2', 'pm25', 'pm10', 'time'], na_values))
     else:
@@ -32,8 +33,9 @@ def update_db():
     soup = BeautifulSoup(page, 'lxml')
     for site in Site.query.all():
         data = validate_data(hourly_data(soup, site.name))
-        site_data = Data(owner=site, **data)
-        db.session.add(site_data)
+        if data:
+            site_data = Data(owner=site, **data)
+            db.session.add(site_data)
     db.session.commit()
 
 
