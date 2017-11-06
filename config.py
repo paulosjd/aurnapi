3a497d8b9dd007f07b048a0cd297394c61c8e24d
Paul Davis <pjdavis@gmx.com>
2017-11-06 22:58:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,19 @@ class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     # For working in PyCharm:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    DEBUG = True
+    TESTING = False
+    MAIL_SERVER = '.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_DEBUG = True
+    MAIL_USERNAME = 'gmail.com'
+    MAIL_PASSWORD = ''
+    MAIL_DEFAULT_SENDER = 'mail.com'
+    MAIL_MAX_EMAILS = None
+    MAIL_SUPPRESS_SEND = False
+    MAIL_ASCII_ATTACHMENTS = False
 
 
 class TestingConfig:
