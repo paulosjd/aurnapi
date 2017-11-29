82cf2d81853652b0979f4d00aed21bb9fcf05257
Paul Davis <pjdavis@gmx.com>
2017-11-29 16:34:35 +0100

    add run.py

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
 from app import create_app
-from flask_migrate import Manager, Migrate, MigrateCommand
-from app.models import db
 
 app = create_app()
 
-migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-
-manager.run()
+if __name__ == '__main__':
+    app.run()
