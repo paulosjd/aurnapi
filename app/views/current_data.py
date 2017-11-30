70f147db0aa0cbf88a19ee5120bf22b7547500fd
Paul Davis <pjdavis@gmx.com>
2017-11-30 10:51:22 +0000

    Update and rename current_data to current_data.py

---
+++ app/views/current_data.py
@@ -0,0 +1,35 @@
+from flask import jsonify, Blueprint
+from app.models import Site, Current
+from app.data.site_info import site_codes, get_info, site_list
+
+
+current_data = Blueprint('current_data', __name__, url_prefix='/current-data')
+
+parameters = {'o3': 'ozone, µg/m-3', 'no2': 'nitrogen dioxide, µg/m-3', 'so2': 'sulfur dioxide, µg/m-3',
+              'pm25': 'PM2.5 particles, µg/m-3', 'pm10': 'PM10 particles, µg/m-3'}
+
+
+def make_site_dict(site_code, current_data_dict):
+    site_names = {i[1]: i[0] for i in site_codes.items()}
+    site_name = site_names.get(site_code)
+    site_dict = {}
+    site_info = get_info(site_name)
+    del site_info['site_code']
+    site_dict['info'] = site_info
+    site_dict['latest_data'] = current_data_dict
+    return site_dict
+
+
+@current_data.route('/all-sites')
+def all_recent_data():
+    all_data = {}
+    def get_current(site_code):
+        current = Current.query.join(Site).filter(Site.site_code == site_code).first()
+        return {'o3': current.o3, 'no2': current.no2, 'so2': current.so2, 'pm25': current.pm25, 'pm10': current.pm10}
+    site_codes_list = [site_codes.get(b) for b in site_list]
+    for a in site_codes_list:
+        all_data[a] = make_site_dict(a, get_current(a))
+    return jsonify(all_data)
+    #catch exceptions e.g. no site name
+
+
