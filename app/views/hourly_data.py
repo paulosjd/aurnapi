e3a4165e4fcfc43b8349fffff36983e43701fc66
Paul Davis <pjdavis@gmx.com>
2017-12-03 09:26:29 +0100

    update view function

--- app/views/hourly_data.py
+++ app/views/hourly_data.py
@@ -14,9 +14,14 @@ parameters = {'o3': 'ozone, µg/m-3', 'no2': 'nitrogen dioxide, µg/m-3', 'so2':
 def hourly_data_new1(site_code, days):
     qs = Data.query.join(Site).filter(Site.site_code == site_code.upper()).order_by(Data.id.desc()).limit(days).all()
     if qs:
+
         times = [a.time for a in qs]
+        data_dict = {a.time: {'no2': a.no2} for a in qs}
         aq_data = {a: {} for a in times}
-        all_data = {site_code.upper(): make_site_dict(site_code.upper(), aq_data)}
+        for a in times:
+            aq_data[a] = {}
+        all_data = {site_code.upper(): make_site_dict(site_code.upper(), data_dict)}
+        #all_data = {site_code.upper(): make_site_dict(site_code.upper(), aq_data)}
 
         return jsonify(all_data)
         #return jsonify({site_code.upper(): 'make_site_dict'})
