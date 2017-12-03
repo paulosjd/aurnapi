84cf7a98aab07a1f78dd962befc9eedf8721988a
Paul Davis <pjdavis@gmx.com>
2017-12-03 09:15:38 +0100

    update views

--- app/views/hourly_data.py
+++ app/views/hourly_data.py
@@ -16,8 +16,9 @@ def hourly_data_new1(site_code, days):
     if qs:
         times = [a.time for a in qs]
         aq_data = {a: {} for a in times}
+        all_data = {site_code.upper(): make_site_dict(site_code.upper(), aq_data)}
 
-        return jsonify({site_code.upper(): make_site_dict(site_code.upper(), aq_data)})
+        return jsonify(all_data)
         #return jsonify({site_code.upper(): 'make_site_dict'})
         #return jsonify({site_code.upper(): {'site_info': {}, 'data': {}}})
     else:
