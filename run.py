0c8a2cf41012274f26dba7fab257b36a05057be0
Paul Davis <pjdavis@gmx.com>
2019-02-26 22:59:44 +0000

    register stats blueprint

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
 import sys
+
 from flask import jsonify
 from flask_login import LoginManager
 from flasgger import Swagger
+
 from app.models import User
 from app import create_app
 from app.data.seed_sites import create_db
 from app.data.collect_data import update_db
+from app.misc_views import misc_data
 from app.data_views import hourly_data
 from app.site_views import site_views
 
 application = create_app()
 application.register_blueprint(hourly_data)
+application.register_blueprint(misc_data)
 application.register_blueprint(site_views)
 
 login_manager = LoginManager()
@@ -20,7 +24,6 @@ login_manager.init_app(application)
 def load_user(user_id):
     return User.get(user_id)
 
-
 @login_manager.request_loader
 def load_user_from_request(request):
     api_key = request.headers.get('Authorization')
@@ -28,11 +31,9 @@ def load_user_from_request(request):
         return None
     return User.query.filter_by(api_key=api_key).first()
 
-
 @application.errorhandler(404)
 def not_found(error):
     return jsonify({'message': str(error)}), 404
-
 
 @application.errorhandler(401)
 def unauthorized(error):
