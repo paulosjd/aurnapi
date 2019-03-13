29a6941df68174b8df9a5a4fc34793fcd8392c14
Paul Davis <pjdavis@gmx.com>
2019-03-13 23:13:14 +0000

    Update misc_views.py

--- app/misc_views.py
+++ app/misc_views.py
@@ -11,22 +11,47 @@ from app.schemas import current_hour_schema
 
 misc_data = Blueprint('misc_data', __name__, url_prefix='/stats')
 
+
+@misc_data.route('/highest/<pollutant>')
 @misc_data.route('/highest/<pollutant>/<int:num>')
 def highest(pollutant=None, num=None):
     field = getattr(HourlyData, pollutant) or HourlyData.pm10
-    data = HourlyData.recent().filter(field != '')
-    data = data.order_by(desc(cast(field, Integer))).limit(num or 10)
-    return current_hour_schema.jsonify(data)
+    data = HourlyData.recent().order_by(field.cast(db.Integer).desc()).limit(num or 10)
+    grouped = {}
+    for a in data:
+        date = a.time.split(' ')[0]
+        date_val_pair = '-'.join(date.split('/')[::-1]), getattr(a, pollutant, 0)
+        grouped.setdefault(a.owner.name, []).append(date_val_pair)
+    json_data = {}
+    for key in grouped:
+        json_data[key] = {}
+        counter = Counter([i[0] for i in grouped[key]])
+        for date, val in grouped[key]:
+            json_data[key][date] = {'counts': counter[date],
+                                    'max': max([int(i[1]) for i in grouped[key] if i[1].isdigit()])}
+    return jsonify(json_data)
+
 
 @misc_data.route('/highest-sites/<pollutant>')
 @misc_data.route('/highest-sites/<pollutant>/<int:num>')
 def highest_sites(pollutant=None, num=None):
     field = getattr(HourlyData, pollutant) or HourlyData.pm10
-    data = HourlyData.recent().join(Site).filter(field != '')
+    data = HourlyData.query.join(Site).filter(field != '')
     data = data.order_by(desc(cast(field, Integer))).limit(num or 500)
     grouped = {}
     for a in data:
         date = a.time.split(' ')[0]
-        grouped.setdefault(a.owner.name, []).append('-'.join(date.split('/')[::-1]))
-    return jsonify(sorted([{'name': k, 'date_counts': Counter(v)} for k, v in 
-                           grouped.items()], key=itemgetter('name')))
+        date_val_pair = '-'.join(date.split('/')[::-1]), getattr(a, pollutant, 0)
+        grouped.setdefault(a.owner.name, []).append(date_val_pair)
+    json_data = {}
+    for key in grouped:
+        json_data[key] = {}
+        counter = Counter([i[0] for i in grouped[key]])
+        for date, val in grouped[key]:
+            json_data[key][date] = {'count': counter[date],
+                                    'max': max([int(i[1]) for i in grouped[key] if i[1].isdigit() and i[0] == date])}
+    return jsonify(
+        sorted([{'name': k, 'date_counts': json_data[k]} for k, v in grouped.items()],
+               key=itemgetter('name'))
+    )
+
